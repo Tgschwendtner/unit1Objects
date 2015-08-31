12f29747e8c92c5442a4cf0a8e913ec5c3651dcd
Tgschwendtner <tgschwendtner@NN121-HA-14.HS.sd203.org>
2015-08-31 10:28:21 -0500

    8/31/15

---
+++ Chapter 2 Practice/E2_4.java
@@ -0,0 +1,9 @@
+import java.awt.Rectangle;
+public class E2_4
+{
+   public static void main(String[] args)
+   {
+       Rectangle box = new Rectangle(5,10,20,30);
+       box.add(0,0);
+   }
+}
--- Chapter 2 Practice/RectanglePrinter.java
+++ Chapter 2 Practice/RectanglePrinter.java
@@ -5,7 +5,7 @@ public class RectanglePrinter
    public static void main(String[] args)
    {
       Rectangle r1 = new Rectangle(0, 0, 100, 50);
-      Rectangle r2 = r1;
+      Rectangle r2 = new Rectangle(0, 0, 100, 50);
       r2.grow(10,20);
       System.out.println(r1);
       System.out.println(r2);
